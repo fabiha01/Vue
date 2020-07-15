c3a2a73c8c9efae14e39d771a6e8cb6a57abab8d
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-07-15 14:00:54 +0100

    create http-req app

---
+++ vue/http-req/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <input type="text">
+    <users my-name="Fabiha"/>
+  </div>
+</template>
+
+<script>
+import Users from './components/UsersList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Users
+  },
+  data() {
+    return {
+      id: '',
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
