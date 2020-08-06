6efbb31b6947f19b26f6c342c9f73eb50f9c005c
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-08-06 21:36:39 +0100

    add data for card, add styles to card

--- vue/http-req/src/components/GridTable.vue
+++ vue/http-req/src/components/GridTable.vue
@@ -3,26 +3,28 @@
         <h1 class="subheading">Dashboard</h1>
 
         <v-container class="my-5">
-            <v-card flat outlined class="pa-3">
-                <v-layout row wrap>
+            <v-card flat v-for="project in projects" :key="project.title">
+                <v-layout row wrap :class="`pa-3 project ${project.status}`">
                     <v-flex xs12 md6>
                         <div class="caption">Project Title</div>
-                        <div>Create a new website</div>
+                        <div>{{project.title}}</div>
                     </v-flex>
                     <v-flex xs6 sm4 md2>
                         <div class="caption">Person</div>
-                        <div>The Net Ninja</div>
+                        <div>{{project.person}}</div>
                     </v-flex>
                     <v-flex xs6 sm4 md2>
                         <div class="caption">Due By</div>
-                        <div>1st Jan 2020</div>
+                        <div>{{project.due}}</div>
                     </v-flex>
                     <v-flex xs2 sm4 md2>
                         <div class="caption">Status</div>
-                        <div>Ongoing</div>
+                        <div>{{project.status}}</div>
                     </v-flex>
                 </v-layout>
+            <v-divider></v-divider>
             </v-card>
+
 
         </v-container>
     </v-app>
@@ -30,7 +32,30 @@
 
 <script>
 export default {
-    name: 'GridTable'
+    data() {
+        return {
+            projects: [
+                { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+                { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+                { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+                { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+            ]
+        }
+    }
     
 }
 </script>
+<style scoped>
+.project.complete{
+    border-left: 4px solid #3cd1c2;
+}
+
+.project.ongoing{
+    border-left: 4px solid orange;
+}
+
+.project.overdue{
+    border-left: 4px solid tomato;
+}
+
+</style>
